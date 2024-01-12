101f7cef0748920e092e42f074cd49ca5cbaf008
bruno-br <bruno.brasilf@gmail.com>
2024-01-12 11:00:53 -0300

    check for KEY_DOWN instead of action "ui_down" on SearchInput

--- addons/script_search/src/SearchInput.gd
+++ addons/script_search/src/SearchInput.gd
@@ -4,8 +4,15 @@ func _ready():
 	grab_focus()
 
 func _unhandled_key_input(event: InputEvent):
-	if event.is_action("ui_down") && has_focus():
+	if _should_focus_next_element(event):
 		_focus_next_element()
+
+func _should_focus_next_element(event: InputEvent) -> bool:
+	return (
+		event is InputEventKey
+		&& event.get_keycode() == KEY_DOWN
+		&& has_focus()
+	) 
 
 func _focus_next_element():
 	var next_element = find_next_valid_focus()
