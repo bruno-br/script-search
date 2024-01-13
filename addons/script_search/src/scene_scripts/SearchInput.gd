18c1dd3651bfa34d8fff8609fa877987235b4ec0
bruno-br <bruno.brasilf@gmail.com>
2024-01-13 20:18:16 -0300

    block caret moving while selecting file with arrow keys

--- addons/script_search/src/scene_scripts/SearchInput.gd
+++ addons/script_search/src/scene_scripts/SearchInput.gd
@@ -5,19 +5,47 @@ extends LineEdit
 signal highlight_prev
 signal highlight_next
 
-func _unhandled_key_input(event: InputEvent):
-	if _should_highlight_next_element(event):
-		emit_signal("highlight_next")
-	if _should_highlight_prev_element(event):
-		emit_signal("highlight_prev")
+const INVISIBLE := Color("00000000")
+
+var _prev_caret_column := 0
+var _is_changing_highlight := false
+
+func _gui_input(event):
+	_update_caret()
+	
+	if _event_is_highlight_next_element(event):
+		_handle_highlight_change(event, "highlight_next")
+	elif _event_is_highlight_prev_element(event):
+		_handle_highlight_change(event, "highlight_prev")
+	
 	grab_focus()
 
-func _should_highlight_next_element(event: InputEvent) -> bool:
-	return (
-		event is InputEventKey && event.get_keycode() == KEY_DOWN
-	)
+func _update_caret():
+	if self._is_changing_highlight:
+		_hide_caret()
+		set_caret_column(self._prev_caret_column)
+	else:
+		_show_caret()
+		self._prev_caret_column = get_caret_column()
 
-func _should_highlight_prev_element(event: InputEvent) -> bool:
-	return (
-		event is InputEventKey && event.get_keycode() == KEY_UP
-	)
+func _hide_caret():
+	if not has_theme_color_override("caret_color"):
+		add_theme_color_override("caret_color", INVISIBLE)
+
+func _show_caret():
+	if has_theme_color_override("caret_color"):
+		remove_theme_color_override("caret_color")
+
+func _event_is_highlight_next_element(event: InputEvent) -> bool:
+	return (event is InputEventKey && event.get_keycode() == KEY_DOWN)
+
+func _event_is_highlight_prev_element(event: InputEvent) -> bool:
+	return (event is InputEventKey && event.get_keycode() == KEY_UP)
+
+func _handle_highlight_change(event, signal_name):
+	if event.is_pressed():
+		self._is_changing_highlight = true
+	else:
+		self._is_changing_highlight = false
+		emit_signal(signal_name)
+	_update_caret()
