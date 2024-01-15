e2007495756cf49140d066b44570d7a1a5acf915
bruno-br <bruno.brasilf@gmail.com>
2024-01-15 18:32:31 -0300

    refactor FileButtons

---
+++ addons/script_search/src/FileButtonCollection.gd
@@ -0,0 +1,49 @@
+var _all := []
+var _visible := []
+
+func _init(all=[]):
+	self._all = all
+
+func append(element):
+	if element.is_visible(): self._visible.append(element)
+	self._all.append(element)
+
+func has_visible() -> bool:
+	return not self._visible.is_empty()
+
+func update(value: Array):
+	self._all = value
+	update_visible_elements()
+
+func update_visible_elements():
+	self._visible = self._all.filter(
+		func(button): return button.is_visible()
+	)
+
+func get_first_visible():
+	if has_visible(): return self._visible[0]
+	return null
+
+func get_next_visible(element):
+	if element == null: return get_first_visible()
+	
+	var pos = _find_element_position(element, self._visible)
+	var next = pos + 1 if pos + 1 < self._visible.size() else 0
+	
+	return self._visible[next]
+
+func get_prev_visible(element):
+	if element == null: return get_first_visible()
+	
+	var pos = _find_element_position(element, self._visible)
+	var prev = pos - 1 if pos - 1 >= 0 else self._visible.size() - 1
+	
+	return self._visible[prev]
+
+func _find_element_position(element, array) -> int:
+	return array.bsearch_custom(
+		element.get_file_name(), _is_file_button_name_lower
+	)
+	
+func _is_file_button_name_lower(file_button, searched_text) -> bool:
+	return (file_button.get_file_name() < searched_text)
--- addons/script_search/src/scene_scripts/FileButtons.gd
+++ addons/script_search/src/scene_scripts/FileButtons.gd
@@ -5,12 +5,13 @@ extends VBoxContainer
 signal script_selected
 signal search_text_updated
 
+const FileButtonCollection := preload("res://addons/script_search/src/FileButtonCollection.gd")
 const FileButtonScene := preload('res://addons/script_search/scenes/FileButton.tscn')
+
 const SEARCH_MATCH_TIME := 0.05
 const BUTTONS_UPDATE_TIME := 0.02
 
-var _buttons := []
-var _visible_buttons := []
+var _file_buttons = FileButtonCollection.new()
 var _highlighted_button = null
 
 var _search_text: String = ""
@@ -19,32 +20,31 @@ var _visible_buttons_update_timer: SceneTreeTimer = null
 
 func open(files: Array):
 	_add_buttons(files)
-	if not self._visible_buttons.is_empty(): 
-		_highlight_file_button(self._visible_buttons[0])
+	_highlight_file_button(self._file_buttons.get_first_visible())
 
 func close():
 	_clear_buttons()
 
-func _add_buttons(files: Array):
-	for file in files:
-		var file_button = FileButtonScene.instantiate()
-		file_button.set_file_name(file)
-		file_button.script_selected.connect(_on_script_selected)
-		file_button.button_hovered.connect(_on_button_hovered)
-		file_button.visibility_changed.connect(_on_button_visibility_changed)
-		search_text_updated.connect(file_button._on_search_text_updated)
-		add_child(file_button)
-		
-		if file_button.is_visible():
-			self._visible_buttons.append(file_button)
-		self._buttons.append(file_button)
+func _add_buttons(file_names: Array):
+	for file_name in file_names: _add_button(file_name)
+
+func _add_button(file_name):
+	var file_button = FileButtonScene.instantiate()
+	file_button.set_file_name(file_name)
+	
+	file_button.script_selected.connect(_on_script_selected)
+	file_button.button_hovered.connect(_on_button_hovered)
+	file_button.visibility_changed.connect(_on_button_visibility_changed)
+	search_text_updated.connect(file_button._on_search_text_updated)
+	
+	add_child(file_button)
+	self._file_buttons.append(file_button)
 
 func _clear_buttons():
 	for child in get_children():
 		child.hide()
 		child.queue_free()
-	self._buttons = []
-	self._visible_buttons = []
+	self._file_buttons.update([])
 
 func _on_script_selected(script):
 	emit_signal("script_selected", script)
@@ -53,46 +53,28 @@ func _on_button_hovered(file_button):
 	_highlight_file_button(file_button)
 
 func _on_highlight_prev():
-	if self._visible_buttons.is_empty(): 
-		return
-	
-	if self._highlighted_button == null:
-		_highlight_file_button(self._visible_buttons[0])
-		return
-	
-	var file_name = self._highlighted_button.get_file_name()
-
-	var expected_pos = self._visible_buttons.bsearch_custom(file_name, _is_file_button_name_lower)
-	
-	_highlight_file_button(self._visible_buttons[expected_pos - 1])
+	_move_highlight("get_prev_visible")
 
 func _on_highlight_next():
-	if self._visible_buttons.is_empty():
-		return
-	
-	if self._highlighted_button == null:
-		_highlight_file_button(self._visible_buttons[0])
-		return
-	
-	var file_name = self._highlighted_button.get_file_name()
-	var expected_pos = self._visible_buttons.bsearch_custom(file_name, _is_file_button_name_lower)
-	
-	var next = expected_pos + 1
-	if next >= self._visible_buttons.size(): next = 0
-	_highlight_file_button(self._visible_buttons[next])
+	_move_highlight("get_next_visible")
 
-func _is_file_button_name_lower(file_button, searched_text) -> bool:
-	return (file_button.get_file_name() < searched_text)
+func _move_highlight(method):
+	if not self._file_buttons.has_visible(): return
+	
+	_highlight_file_button(
+		self._file_buttons.call(method, self._highlighted_button)
+	)
 
 func _highlight_file_button(file_button):
-	if self._highlighted_button == file_button: 
-		return
+	if self._highlighted_button == file_button: return
 	
 	if self._highlighted_button != null:
 		self._highlighted_button.set_highlight(false)
 	
-	file_button.set_highlight(true)
-	get_parent().ensure_control_visible(file_button)
+	if file_button != null:
+		file_button.set_highlight(true)
+		get_parent().ensure_control_visible(file_button)
+	
 	self._highlighted_button = file_button
 
 func _on_selected():
@@ -112,17 +94,18 @@ func _on_search_input_text_changed(new_text):
 		self._search_update_timer = null
 
 func _on_button_visibility_changed():
-	if self._highlighted_button != null && not self._highlighted_button.is_visible():
-		self._highlighted_button.set_highlight(false)
-		self._highlighted_button = null
+	if _highlighted_button_is_invisible(): _highlight_file_button(null)
 	
 	if self._visible_buttons_update_timer == null:
 		self._visible_buttons_update_timer = get_tree().create_timer(BUTTONS_UPDATE_TIME)
 		
 		await self._visible_buttons_update_timer.timeout
 		
-		self._visible_buttons = self._buttons.filter(
-			func(button): return button.is_visible()
-		)
-		
+		self._file_buttons.update_visible_elements()
 		self._visible_buttons_update_timer = null
+
+func _highlighted_button_is_invisible() -> bool:
+	return (
+		self._highlighted_button != null 
+		&& not self._highlighted_button.is_visible()
+	)
