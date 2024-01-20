5bd268e61674a9d8afa39e580fc1b4e899fd3c54
bruno-br <bruno.brasilf@gmail.com>
2024-01-20 15:24:11 -0300

    add description on ConfigItems

--- addons/script_search/src/scene_scripts/ConfigMenu/ConfigItem.gd
+++ addons/script_search/src/scene_scripts/ConfigMenu/ConfigItem.gd
@@ -9,6 +9,9 @@ extends Control
 		get = get_param_key, 
 		set = set_param_key
 
+@export var param_description := "param description":  
+		set = set_param_description
+
 func set_param_name(value: String): 
 	param_name = value
 	$Label.set_text(value + ": ")
@@ -18,6 +21,10 @@ func get_param_key() -> String:
 
 func set_param_key(value: String): 
 	param_key = value
+
+func set_param_description(value: String): 
+	param_description = value
+	set_tooltip_text(value)
 
 func get_param_value() -> String:
 	return $TextEdit.get_text()
