171670ef195471df00f6603bf6e0d27df29337f2
bruno-br <bruno.brasilf@gmail.com>
2024-03-05 11:38:26 -0300

    rename simple_name

--- addons/script_search/src/scene_scripts/SearchMenu/FileButton.gd
+++ addons/script_search/src/scene_scripts/SearchMenu/FileButton.gd
@@ -18,9 +18,9 @@ func remove():
 
 func set_file_name(full_name: String):
 	self._file_name = full_name
-	var simple_name = full_name.get_file()
+	var base_name = full_name.get_file()
 	
-	$Button/FileNameLabel.set_text(simple_name)
+	$Button/FileNameLabel.set_text(base_name)
 	$Button/FilePathLabel.set_text(full_name.trim_prefix("res://"))
 
 func select():
